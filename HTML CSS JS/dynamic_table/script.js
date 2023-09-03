950c4e28fed613b762afdef8c6a39cbe82ff18d5
Muhammad Habib Khan <muhammad2002khan@gmail.com>
2023-09-03 14:33:44 +0500

    empty

--- HTML CSS JS/dynamic_table/script.js
+++
@@ -1,7 +0,0 @@
-function addRow(){
-    const mytable = document.getElementById("table_id")
-    const newRow = mytable.insertRow()
-    const cell1 = newRow.insertCell()
-
-    cell1.innerHTML = "4"
-}
